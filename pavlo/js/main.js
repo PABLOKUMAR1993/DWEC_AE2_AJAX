6f495e6717bbbb4dbc61e5b280dc4015900a564f
Pavlo Dudnyk <pablokumar1993@gmail.com>
2022-11-08 13:41:30 +0100

    Se muestran los datos de AJAX en la página

--- pavlo/js/main.js
+++ pavlo/js/main.js
@@ -1,10 +1,18 @@
+// Datos de conexión al server.
+
 const URL_SERVER = "http://127.0.0.1:5500/";
 const RECURSO1 = "pavlo/data/tamanyo.json";
 const RECURSO2 = "pavlo/data/ingredientes.json";
 
-// Está función conecta con el server y obtiene l
+// Llamo a la función que trae la información del server, cuando se ha terminado de cargar la página.
+
+window.onload = datosAjax();
+
+// Está función conecta con el server y obtiene la información de dos ficheros .json.
 
 function datosAjax() {
+
+    // Fichero 1:
 
     let xmlHttpTamanyos = new XMLHttpRequest();
 
@@ -13,11 +21,14 @@ function datosAjax() {
     xmlHttpTamanyos.onreadystatechange = function() {
         if (this.readyState == 4) {
             if (this.status == 200) {
+                // Si la conexión es exitosa, le paso a la siguiente función la información obtenida.
                 mostrarTamanyos(this.responseText);
             }        
         }
     }
     
+    // Fichero 2:
+
     let xmlHttpIngredientes = new XMLHttpRequest();
 
     xmlHttpIngredientes.open('GET', URL_SERVER + RECURSO2, true);
@@ -25,6 +36,7 @@ function datosAjax() {
     xmlHttpIngredientes.onreadystatechange = function() {
         if (this.readyState == 4) {
             if (this.status == 200) {
+                // Si la conexión es exitosa, le paso a la siguiente función la información obtenida.
                 mostrarIngredientes(this.responseText);
             }        
         }
@@ -32,18 +44,60 @@ function datosAjax() {
 
 }
 
+// Esta función crea los elementos en el DOM para mostrar los tamaños de las pizzas.
+
 function mostrarTamanyos(jsonDoc) {
 
     let objeto = JSON.parse(jsonDoc);
-    console.log("TAMAÑOS");
-    console.log(objeto);
+    
+    let label = [];
+    let labelTx = [];
+    let input = [];
+
+    for (let i=0; i<objeto.TAMAÑO.length; i++) {
+
+        label[i] = document.createElement("label");
+        labelTx[i] = document.createTextNode(`${objeto.TAMAÑO[i].name}`);
+        input[i] = document.createElement("input");
+
+        sectionTamanyo.appendChild(label[i]);
+
+        label[i].appendChild(labelTx[i]);
+        label[i].appendChild(input[i]);
+
+        input[i].setAttribute("type", "radio");
+        input[i].setAttribute("name", "tamanyo");
+        input[i].value = `${objeto.TAMAÑO[i].value}`;
+
+    }
 
 }
+
+// Esta función crea los elementos en el DOM para mostrar los ingredientes para las pizzas.
 
 function mostrarIngredientes(jsonDoc) {
 
     let objeto = JSON.parse(jsonDoc);
-    console.log("INGREDIENTES");
-    console.log(objeto);
+    
+    let label = [];
+    let labelTx = [];
+    let input = [];
+
+    for (let i=0; i<objeto.INGREDIENTES.length; i++) {
+
+        label[i] = document.createElement("label");
+        labelTx[i] = document.createTextNode(`${objeto.INGREDIENTES[i].name}`);
+        input[i] = document.createElement("input");
+
+        sectionIngredientes.appendChild(label[i]);
+
+        label[i].appendChild(labelTx[i]);
+        label[i].appendChild(input[i]);
+
+        input[i].setAttribute("type", "checkbox");
+        input[i].setAttribute("name", "ingrediente");
+        input[i].value = `${objeto.INGREDIENTES[i].value}`;
+
+    }
 
 }
